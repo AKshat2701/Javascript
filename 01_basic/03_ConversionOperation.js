2bb179c737f9b2f0f7d5f5a022cdb0fe01b20306
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2024-08-16 06:11:41 +0000

    string with charactets

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -15,5 +15,33 @@ let booleanIsLoggedIn =  Boolean(isLoggedIn)
 let someNumber= 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// *******/ operator /********** */
+
+let value  = 4
+let negValue =  -value ;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2**2)
+// console.log(2%2)
+// console.log(2%3)
+
+let str1 =  "HELLO ";
+let str2 = "Akshat";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 )
+// console.log("1" + "2" )
+// console.log(1 + "2" + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true);
+console.lo(+"")
