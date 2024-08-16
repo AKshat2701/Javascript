a121271b02672bd62f9b8a92a597bcf6aa990a95
AKshat srivastava <137937151+AKshat2701@users.noreply.github.com>
2024-08-16 03:35:57 +0000

    converstions

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,19 @@
+let score = "akshat"
+
+// const {score} = req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "" ;
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber= 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
